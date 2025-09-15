f10a1cce247ba394c1b8e318859657d8f39b17ea
Miriam1025 <miriamlpappas@gmail.com>
2025-09-15 15:40:16 -0700

    cvbx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SalesPage from './SalesPage';
 import Examples from './Examples';
+import BookmarkBuilder from './BookmarkBuilder';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SalesPage />} />
           <Route path="/examples" element={<Examples />} />
+          <Route path="/builder" element={<BookmarkBuilder />} />
           <Route path="*" element={<SalesPage />} />
         </Routes>
       </div>
